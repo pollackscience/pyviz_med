c1d0c31115784674dddbe72cf59667883b6e4fe7
Brian Pollack <brianleepollack@gmail.com>
2020-05-21 17:43:14 -0400

    three_axis and modality updated

--- pyviz_med/visual.py
+++ pyviz_med/visual.py
@@ -18,21 +18,41 @@ class MedImage:
 
     def __init__(self, path, img_type='infer', *args, **kwargs):
         '''Initialize via reading the image and creating the xarray.'''
-        self.read_image(path, img_type)
+        np_img_list = []
+        if type(path) is list:
+            n_mod = len(path)
+            for p in path:
+                np_img_list.append(self.read_image(p, img_type))
+        else:
+            n_mod = 1
+            np_img_list = [self.read_image(path, img_type)]
+
+        max_z = max_y = max_x = 0
+        for img in np_img_list:
+            max_z = max(img.shape[0], max_z)
+            max_y = max(img.shape[1], max_y)
+            max_x = max(img.shape[2], max_x)
+        pad = np.zeros((max_z, max_y, max_x))
+        print(pad.shape)
+        for i,img in enumerate(np_img_list):
+            pad_copy = pad.copy()
+            pad_copy[:img.shape[0], :img.shape[1], :img.shape[2]]=img
+            np_img_list[i] = pad_copy
+
+        np_img_list = np.stack(np_img_list, axis=0)
 
         self.subject = kwargs.get('subject', 'Unnamed')
-        self.modality = kwargs.get('modality', 'Unknown')
+        self.modality = kwargs.get('modality', ['Unknown']*n_mod)
 
-        print(self.img_np.shape)
+        print(np_img_list.shape)
         self.ds = xr.Dataset({'image': (['subject', 'modality', 'z', 'y', 'x'],
-                                        self.img_np[np.newaxis, np.newaxis, :])
-                              },
-
+                                        np_img_list[np.newaxis, :])
+                             },
                              coords={'subject': [self.subject],
-                                     'modality': [self.modality],
-                                     'z': range(self.img_np.shape[0]),
-                                     'y': range(self.img_np.shape[1]),
-                                     'x': range(self.img_np.shape[2])
+                                     'modality': self.modality,
+                                     'z': range(np_img_list.shape[1]),
+                                     'y': range(np_img_list.shape[2])[::-1],
+                                     'x': range(np_img_list.shape[3])
                                     }
                              )
 
@@ -62,7 +82,7 @@ class MedImage:
         elif img_type == 'dicom':
             reader = sitk.ImageSeriesReader()
             print(str(path))
-            dicom_names = reader.GetGDCMSeriesFileNames(str(path)[:-5])
+            dicom_names = reader.GetGDCMSeriesFileNames(str(path))
             print(dicom_names)
             dicom_names = sorted(dicom_names, key=lambda a: Path(a).stem[2:].zfill(3))
             reader.SetFileNames(dicom_names)
@@ -73,85 +93,64 @@ class MedImage:
             print('origin', image.GetOrigin())
             print('spacing', image.GetSpacing())
 
-        self.img_np = sitk.GetArrayFromImage(image)
+        return sitk.GetArrayFromImage(image)
 
 
-    def view(self):
+    def view(self, three_axis=False):
+        imopts = {'tools': ['hover', 'lasso_select'], 'width': 400, 'height': 400, 'cmap': 'gray'}
+        opts.defaults(
+            opts.GridSpace(shared_xaxis=True, shared_yaxis=True,
+                           fontsize={'title': 16, 'labels': 16, 'xticks': 12,
+                                     'yticks': 12},
+                           plot_size=300),
+        )
         hv_ds = hv.Dataset(self.ds)
-        return hv_ds.to(hv.Image, ['x', 'y'], groupby='z', dynamic=True)
 
+        cslider = pn.widgets.RangeSlider(start=-3000, end=3000, value=(-800, 800), name='contrast')
+        if three_axis:
+            gridspace = hv.GridSpace(kdims=['plane', 'modality'], label=f'{self.subject}')
+            for mod in self.modality:
+                gridspace['axial', mod] = hv_ds.select(modality=mod).to(
+                    hv.Image, ['x', 'y'], groupby=['z'],
+                    dynamic=True).opts(**imopts).apply.opts(clim=cslider.param.value)
+                gridspace['coronal', mod] = hv_ds.select(modality=mod).to(
+                    hv.Image, ['x', 'z'], groupby=['y'],
+                    dynamic=True).opts(**imopts).apply.opts(clim=cslider.param.value)
+                gridspace['sagittal', mod] = hv_ds.select(modality=mod).to(
+                    hv.Image, ['y', 'z'], groupby=['x'],
+                    dynamic=True).opts(**imopts).apply.opts(clim=cslider.param.value)
 
-
-def patient_series_viewer(path, patient, img_type='DICOM', info=''):
-    '''Similar to pybreast viewer, but must natively handle a mix of 2d, 3d, scalar, and vector'''
-
-    imopts = {'tools': ['hover', 'lasso_select'], 'width': 300, 'height': 300, 'cmap': 'viridis'}
-    full_path = Path(path, patient)
-
-    if img_type == 'NIFTI':
-        img_folders = sorted(list(full_path.glob('*.nii')), key=lambda a: a.stem.split('_'))
-        reader = sitk.ImageFileReader()
-        reader.SetImageIO("NiftiImageIO")
-    elif img_type == 'DICOM':
-        img_folders = sorted(list(full_path.iterdir()), key=lambda a: int(a.stem[2:]))
-        reader = sitk.ImageSeriesReader()
-    elif img_type == 'DICOM_CHAOS':
-        img_folders = [Path(full_path, 'T1DUAL/DICOM_anon/InPhase'),
-                       Path(full_path, 'T1DUAL/DICOM_anon/OutPhase'),
-                       Path(full_path, 'T2SPIR/DICOM_anon')]
-        reader = sitk.ImageSeriesReader()
-    elif img_type == 'DICOM_CHAOS_CT':
-        img_folders = [Path(full_path, 'DICOM_anon')]
-        reader = sitk.ImageSeriesReader()
-
-    else:
-        raise KeyError(f'img_type must be one of ["DICOM", "NIFTI"], got {img_type}')
-
-    hv_images = []
-    for img_files in img_folders:
-        print(img_files)
-        hvds_list = []
-        if 'DICOM' in img_type:
-            dicom_names = reader.GetGDCMSeriesFileNames(str(img_files))
-            dicom_names = sorted(dicom_names, key=lambda a: Path(a).stem[2:].zfill(3))
-            reader.SetFileNames(dicom_names)
-            reader.MetaDataDictionaryArrayUpdateOn()  # Get DICOM Info
-            reader.LoadPrivateTagsOn()  # Get DICOM Info
-            image = reader.Execute()
-            print('direction', image.GetDirection())
-            print('origin', image.GetOrigin())
-            print('spacing', image.GetSpacing())
-            desc = reader.GetMetaData(0, '0008|103e').strip().encode('utf-8', 'ignore').decode()
-            pid = reader.GetMetaData(0, '0010|0010').strip()
-            desc = ' '.join([img_files.stem, desc, pid])
-            # if image.GetOrigin()[-1] > 0:
-            #     image = image[:, :, ::-1]
-        elif img_type == 'NIFTI':
-            reader.SetFileName(str(img_files))
-            desc = ' '.join(img_files.parts[-2:])
-            image = reader.Execute()
-            print('direction', image.GetDirection())
-            print('origin', image.GetOrigin())
-            print('spacing', image.GetSpacing())
-
-        npimg = sitk.GetArrayFromImage(image)
-        print(npimg.shape)
-        if npimg.shape[0] == 1:
-            hv_images.append(hv.Image(npimg[0, :], label=desc).opts(**imopts))
-        elif npimg.shape[-1] > 3:
-            hvds_list.append(hv.Dataset(
-                (np.arange(npimg.shape[2]), np.arange(npimg.shape[1])[::-1],
-                 np.arange(npimg.shape[0]),
-                 npimg), [f'x{desc}', f'y{desc}', f'z{desc}'],
-                f'MRI{desc}'))
-            print(hvds_list[-1])
-            hv_images.append(hvds_list[-1].to(hv.Image, [f'x{desc}', f'y{desc}'],
-                                              groupby=[f'z{desc}'],
-                                              dynamic=True, label=desc).opts(**imopts,
-                                                                             invert_yaxis=False))
         else:
-            hv_images.append(hv.Image(npimg[0, :], label=desc).opts(**imopts))
-        print()
-    return hv.Layout(hv_images).opts(shared_axes=False, merge_tools=False, normalize=False,
-                                     title=' '.join([patient, info])).cols(3)
+            gridspace = hv.GridSpace(kdims=['modality'], label=f'self.subject')
+            for mod in self.modality:
+                gridspace[mod] = hv_ds.select(modality=mod).to(
+                    hv.Image, ['x', 'y'], groupby=['z'],
+                    dynamic=True).opts(**imopts).apply.opts(clim=cslider.param.value)
 
+        # hv_image = hv_ds.to(hv.Image, ['x', 'y'], groupby=['z', 'modality'], dynamic=True,
+        #                     label=f'subject={self.subject}', group=f'modality={self.modality}').opts(**imopts)
+        # if three_axis:
+        #     hv_image_2 = hv_ds.to(hv.Image, ['x', 'z'], groupby=['y', 'modality'], dynamic=True).opts(**imopts)
+        #     hv_image_3 = hv_ds.to(hv.Image, ['y', 'z'], groupby=['x', 'modality'], dynamic=True).opts(**imopts)
+        #     img_list = [hv_image, hv_image_2, hv_image_3]
+        # else:
+        #     img_list = [hv_image]
+
+        # Apply sliders
+        # cslider = pn.widgets.RangeSlider(start=-3000, end=3000, value=(-800, 800), name='contrast')
+        # for i in range(len(img_list)):
+        #     img_list[i] = img_list[i].apply.opts(clim=cslider.param.value)
+
+        # combo_image = reduce(lambda a,b : a+b,img_list)
+
+        # Make widget panel
+        #if len(self.ds.modality)>1:
+        #    print(combo_image)
+        #    layout = combo_image.grid('modality').cols(3)
+        #else:
+        #    layout = combo_image
+        # pn_layout = pn.pane.HoloViews(layout)
+        pn_layout = pn.pane.HoloViews(gridspace)
+        wb = pn_layout.widget_box
+        wb.append(cslider)
+        return pn.Row(wb, pn_layout)
